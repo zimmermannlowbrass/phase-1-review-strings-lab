4fb5dce63a28d2320ae96cdf627080fb0d18466f
James Aidan Zimmermann <zimmermann.lowbrass@gmail.com>
2023-01-11 11:32:01 -0600

    completed labwork

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser = 'Zimmermann';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
